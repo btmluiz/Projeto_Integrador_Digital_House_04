dc532f7b6555a0027385f1b2e5dbb6aa93a0139b
Luiz Felipe <btmluiz@outlook.com>
2021-01-29 20:29:02 -0300

    Initial commit

---
+++ app/src/main/java/br/dev/luizfelipe/projetointegradordigitalhouse04/ui/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package br.dev.luizfelipe.projetointegradordigitalhouse04.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import br.dev.luizfelipe.projetointegradordigitalhouse04.R
+import br.dev.luizfelipe.projetointegradordigitalhouse04.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
